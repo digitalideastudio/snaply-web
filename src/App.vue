99ee598415dd2409f830edb36a0b4c03b56a63b5
Serhii Matrunchyk <serhii@digitalidea.studio>
2020-02-12 02:57:10 +0200

    Shinked image

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  html, body
+    padding 0
+    margin 0
+    height 100%
+
+  #app
+    font-family Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    height 100%
+    background #263238
+    padding 0
+    margin 0
+
+  #nav {
+    padding 10px
+
+    a {
+      color #F78C6C
+    }
+  }
 </style>
